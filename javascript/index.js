b6ee7ba08f0ed0b8269acac3aeacadae2368270f
Kaustubh Verma <vermakaustubh28@gmail.com>
2021-03-26 01:41:21 +0530

    added features

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,4 @@
+var x=1;
 $(document).ready(function() {
 
     $("#hide").hover(function(){
@@ -7,6 +8,47 @@ $(document).ready(function() {
     $(".banner").hover(function(){
         $(this).toggleClass('hide2');
         $(".testname").slideDown("slow");
+      });
+      
+      $(".a").click(function(){
+        if(x%2==1){
+          $(".show").slideDown("slow");
+          x++;
+        }else{
+            $(".show").slideUp("slow");
+            x++;
+        }
+        $("showdep").click(function(){
+
+        });
+      });
+      $("#dep").click(function(){
+          $(".h2").text("Depression");
+          $("#showdep").removeClass("showdep")
+          $("#showfear").addClass("showfear")
+          $("#showanger").addClass("showanger");
+          $("#showphone").addClass("showphone");
+      });
+      $("#fear").click(function(){
+        $(".h2").text("Fear");
+        $("#showfear").removeClass("showfear")
+        $("#showdep").addClass("showdep")
+        $("#showanger").addClass("showanger");
+        $("#showphone").addClass("showphone");
+      });
+      $("#anger").click(function(){
+        $(".h2").text("Anger");
+        $("#showanger").removeClass("showanger")
+        $("#showdep").addClass("showdep")
+        $("#showfear").addClass("showfear");
+        $("#showphone").addClass("showphone");
+      });
+      $("#phone").click(function(){
+        $(".h2").text("Phone Addiction");
+        $("#showanger").addClass("showanger")
+        $("#showdep").addClass("showdep")
+        $("#showfear").addClass("showfear");
+        $("#showphone").removeClass("showphone");
       });
 
 });
